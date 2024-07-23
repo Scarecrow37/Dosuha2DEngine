0e639f95b05070eb5cb38258bf00f1f42c422708
Dosuha <weqa123@naver.com>
2024-07-23 19:49:11 +0900

    commit

---
+++ Window/System/ISystem.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+namespace Window
+{
+    struct IClass;
+    struct IHandle;
+
+    struct ISystem : IReferenceCounter 
+    {
+        virtual void Register(LPCWSTR name, IClass** windowClass) const = 0;
+        virtual void Create(IClass* windowClass, RECT windowRect, IHandle** handle) const = 0;
+    };
+}
